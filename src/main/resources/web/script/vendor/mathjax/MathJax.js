36383171eb0787d7c5f90bdf0bab7dcb95bd5380
Malte Reißig <malte@deepamehta.org>
2012-09-27 22:28:55 +0200

    adaptation for deployment

--- src/main/resources/web/script/vendor/mathjax/MathJax.js
+++ src/main/resources/web/script/vendor/mathjax/MathJax.js
@@ -645,8 +645,9 @@ MathJax.fileversion = "2.0";
     fileURL: function (file) {
         if (this.config.root == "") {
             // ### fix config: this quick hack ensures proper loading of menu and jax/ext durign dynamic initialization
-            // http://localhost:8080/de.tu-berlin.eduzen/script/vendor/mathjax
-            this.config.root = "http://stable.eduzen.tu-berlin.de:8080/de.tu-berlin.eduzen/script/vendor/mathjax"
+            // http://localhost:8080/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax
+            this.config.root = "http://stable.eduzen.tu-berlin.de:8080/"
+              + "de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax"
         }
         return file.replace(this.rootPattern, this.config.root)
     },
